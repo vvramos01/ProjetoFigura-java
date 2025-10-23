ca8b3eebcf20e6e14535148d6ca6fd63ca8fd78e
Vitória Ramos <vitoria.ramos@edu.unifil.br>
2025-10-23 20:25:33 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Figura;
+
+
+public class Main {
+   public static void main(String[] args) {
+       Quadrado q = new Quadrado(5);
+       System.out.println(q.calcularArea());
+
+
+       Triangulo t = new Triangulo(6, 2 );
+       System.out.println(t.calcularArea());
+
+
+   }
+
+
+}
